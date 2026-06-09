ce2ad47ca69bfa29a1657521c39e21507b9edab7
Sivam Kumar Dubey <kalyandwivedi49@gmail.com>
2026-06-09 23:24:55 +0530

    Add files via upload

---
+++ DAY_13/question51.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main(){
+    int arr[100],i,n;
+    int largest, smallest;
+
+    printf("enter size of array:");
+    scanf("%d", &n);
+
+    printf("enter %d elements:\n", n);
+
+    for(i=0; i<n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    largest =smallest =arr[0];
+
+    for(i=1; i<n; i++) {
+        if(arr[i]>largest)
+            largest=arr[i];
+
+        if(arr[i]<smallest)
+            smallest=arr[i];
+    }
+
+    printf("largest element=%d\n", largest);
+    printf("smallest element=%d\n", smallest);
+
+    return 0;
+}
