3b5729b9489ebbe1bed5c1398a4ab79fac4870b1
Sivam Kumar Dubey <kalyandwivedi49@gmail.com>
2026-06-18 15:23:40 +0530

    Add files via upload

---
+++ DAY_21/question83.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+    char str[100];
+    int i=0,v=0,c=0;
+
+    fgets(str,sizeof(str),stdin);
+
+    while(str[i]!='\0'){
+
+        if((str[i]>='A'&&str[i]<='Z')||(str[i]>='a'&&str[i]<='z')){
+
+            if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'||
+               str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'||str[i]=='U')
+                v++;
+            else
+                c++;
+        }
+
+        i++;
+    }
+
+    printf("Vowels=%d\n",v);
+    printf("Consonants=%d",c);
+
+    return 0;
+}
